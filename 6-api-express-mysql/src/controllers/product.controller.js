40b1a35ebf5ee0db0cb5fe2fa4e7021741f355ca
Paola Santillan <a.paolasantillangzz@hotmail.com>
2022-09-01 01:20:28 -0500

    Set CRUD data to make modifications to the Database in Postman.

--- 6-api-express-mysql/src/controllers/product.controller.js
+++ 6-api-express-mysql/src/controllers/product.controller.js
@@ -7,7 +7,7 @@ const getProducts = async (req, res) => {
         res.json(result)
     }catch (error){
         console.log(error);
-        res.status(500);
+        res.status(400);
         res.send(error.message)
     }
 }
@@ -21,6 +21,54 @@ const getProduct = async (req, res) => {
         res.json(result)
     }catch (error){
         console.log(error);
+        res.status(400);
+        res.send(error.message)
+    }
+}
+
+const addProduct = async (req, res) => {
+    try{
+        const { id, title, total_pages, author, nationality, language, cover_url, editorial } = req.body;
+        if (id === undefined || title === undefined || total_pages === undefined || author === undefined || nationality === undefined || language === undefined || cover_url === undefined || editorial === undefined){
+            res.status(400).json({message:"Bad request. Please fill all field."})
+        }
+        const product = {id, title, total_pages, author, nationality, language, cover_url, editorial};
+        const connection = await getConnection();
+        const result = await connection.query("INSERT INTO books SET = ?", product);
+        res.json(result)
+    }catch (error){
+        console.log(error);
+        res.status(500);
+        res.send(error.message)
+    }
+}
+
+const updateProduct = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const {title, total_pages, author } = req.body;
+        if (title === undefined || total_pages === undefined || author === undefined){
+            res.status(400).json({message:"Bad request. Please fill all field."})
+        }
+        const product = {title, total_pages, author, nationality, language, cover_url, editorial};
+        const connection = await getConnection();
+        const result = await connection.query("UPDATE books SET ? WHERE id = ?", [product, id]);
+        res.json(result)
+    }catch (error){
+        console.log(error);
+        res.status(500);
+        res.send(error.message)
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const connection = await getConnection();
+        const result = await connection.query("DELETE FROM books WHERE id = ?", id);
+        res.json(result)
+    }catch (error){
+        console.log(error);
         res.status(500);
         res.send(error.message)
     }
@@ -29,4 +77,7 @@ const getProduct = async (req, res) => {
 export const methods = {
     getProducts,
     getProduct,
+    addProduct,
+    updateProduct,
+    deleteProduct
 } 
--- 6-api-express-mysql/src/routes/product.routes.js
+++ 6-api-express-mysql/src/routes/product.routes.js
@@ -5,8 +5,8 @@ const router = Router();
 
 router.get("/", productController.getProducts); // GET http://localhost:5000/api-restfull/products/
 router.get("/:id", productController.getProduct); // GET http://localhost:5000/api-restfull/products/:sku
-// router.post("/", productController.addProduct); // POST http://localhost:5000/api-restfull/products/
-// router.put("/:sku", productController.updateProduct); // PUT http://localhost:5000/api-restfull/products/:sku
-// router.delete("/:sku", productController.deleteProduct); // DELETE http://localhost:5000/api-restfull/products/:sku
+router.post("/", productController.addProduct); // POST http://localhost:5000/api-restfull/products/
+router.put("/:id", productController.updateProduct); // PUT http://localhost:5000/api-restfull/products/:sku
+router.delete("/:sku", productController.deleteProduct); // DELETE http://localhost:5000/api-restfull/products/:sku
 
 export default router;
